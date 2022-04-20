88822fad4498fbae0bd0c5ab7c8343cf45d455da
Leonardo <leonardokennedyalvesdelima@gmail.com>
2022-04-20 15:50:51 -0300

    criaçao da primeira tabela do db

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,3 +1,35 @@
 const db = require('../database');
 const { DataTypes } = require('sequelize');
 
+const Products = db.define("Products", {
+    //Cada objeto é uma coluna
+    codigo: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nome: {
+        type: DataTypes.STRING,
+    },
+    descricao: {
+        type: DataTypes.TEXT,
+    },
+    preco: {
+        type:DataTypes.DOUBLE,
+    },
+    estoque: {
+        type: DataTypes.INTEGER,
+    },
+    link_foto: {
+        type: DataTypes.STRING,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+    },
+}, {
+    //Aqui é onde se insere o nome da tabela no Banco de Dados
+    tablename: "productinfo",
+});
